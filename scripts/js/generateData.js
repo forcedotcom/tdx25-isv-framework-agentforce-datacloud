a31cb021ae271dfb43a4813a42e64201cb216d3a
mvogelgesang <mvogelgesang@salesforce.com>
2025-02-23 13:22:43 -0500

    added ability to create timeseries data

--- scripts/js/generateData.js
+++ scripts/js/generateData.js
@@ -21,81 +21,103 @@ function createApplianceMap(applianceFiles) {
   return typeToSerialsMap;
 }
 
-function generateDummyData(columnsConfig, outputFilePath, options = {}) {
-  const data = [];
+function generateDummyData(columnsConfig, outputFolder, options = {}) {
   const {
     keyAttribute = null,
     numRecords = 0,
     timestampMode = "same",
-    baseDate = new Date(new Date().setUTCHours(0, 0, 0, 0))
+    baseDate = new Date(new Date().setUTCHours(0, 0, 0, 0)),
+    degradationDays = 30,
+    degradationColumns = [],
+    degradationProportion = 0.1,
+    timespanDays = 30
   } = options;
 
-  const generateTimestamp = () => {
-    if (timestampMode === "same") {
-      return baseDate.toISOString();
-    } else if (timestampMode === "relative") {
-      const relativeDate = new Date(baseDate);
-      const hours = faker.number.int({ min: 0, max: 23 });
-      const minutes = faker.number.int({ min: 0, max: 59 });
-      relativeDate.setUTCHours(hours, minutes, 0, 0);
-      return relativeDate.toISOString();
-    }
-    return new Date().toISOString();
+  const generateTimestamp = (day) => {
+    const relativeDate = new Date(baseDate);
+    relativeDate.setDate(baseDate.getDate() + day);
+    return relativeDate.toISOString();
   };
 
-  const generateValue = (config) => {
+  const generateValue = (config, day, applyDegradation) => {
     if (Array.isArray(config)) {
       return faker.helpers.arrayElement(config);
     } else if (typeof config === "object" && config.range) {
-      const { min, max, outlierProbability = 0.1, type = 'int' } = config.range;
+      const { min, max, outlierProbability = 0.1, type = "int" } = config.range;
 
       let value;
       if (Math.random() < outlierProbability) {
-        const outlierOffset = Math.random() < 0.5 ? -(max - min) / 2 : (max - min) / 2;
-        value = type === 'int'
-          ? faker.number.int({ min: min + outlierOffset, max: max + outlierOffset })
-          : faker.number.float({ min: min + outlierOffset, max: max + outlierOffset, precision: 0.01 });
+        const outlierOffset =
+          Math.random() < 0.5 ? -(max - min) / 2 : (max - min) / 2;
+        value =
+          type === "int"
+            ? faker.number.int({
+                min: min + outlierOffset,
+                max: max + outlierOffset
+              })
+            : faker.number.float({
+                min: min + outlierOffset,
+                max: max + outlierOffset,
+                precision: 0.01
+              });
       } else {
-        value = type === 'int'
-          ? faker.number.int({ min, max })
-          : faker.number.float({ min, max, precision: 0.01 });
+        value =
+          type === "int"
+            ? faker.number.int({ min, max })
+            : faker.number.float({ min, max, precision: 0.01 });
+      }
+
+      // Apply degradation factor if applicable
+      if (applyDegradation && degradationColumns.includes(config.columnName)) {
+        const degradationFactor = day / degradationDays;
+        value = Math.min(max, value + (max - min) * degradationFactor);
       }
       return value;
     } else {
-      console.error(`Invalid configuration for column ${columnName}`);
+      console.error(`Invalid configuration for column ${config.columnName}`);
       return null;
     }
   };
 
-  if (keyAttribute && columnsConfig[keyAttribute]) {
-    const colConfigWithoutKey = { ...columnsConfig };
-    delete colConfigWithoutKey[keyAttribute];
+  for (let day = 0; day < timespanDays; day++) {
+    const data = [];
+    const currentDate = new Date(baseDate);
+    currentDate.setDate(baseDate.getDate() + day);
+    const dateString = currentDate.toISOString().split("T")[0];
 
-    for (let i = 0; i < columnsConfig[keyAttribute].length; i++) {
-      const record = {};
-      for (const columnName in colConfigWithoutKey) {
-        const config = colConfigWithoutKey[columnName];
-        record[columnName] = generateValue(config);
+    if (keyAttribute && columnsConfig[keyAttribute]) {
+      const colConfigWithoutKey = { ...columnsConfig };
+      delete colConfigWithoutKey[keyAttribute];
+      for (let i = 0; i < columnsConfig[keyAttribute].length; i++) {
+        const record = {};
+        const applyDegradation = Math.random() < degradationProportion;
+        for (const columnName in colConfigWithoutKey) {
+          const config = colConfigWithoutKey[columnName];
+          config.columnName = columnName; // Add column name to config for reference
+          record[columnName] = generateValue(config, day, applyDegradation);
+        }
+        record[keyAttribute] = columnsConfig[keyAttribute][i];
+        record.timestamp = generateTimestamp(day);
+        data.push(record);
       }
-      record[keyAttribute] = columnsConfig[keyAttribute][i];
-      record.timestamp = generateTimestamp();
-      data.push(record);
-    }
-  } else if (numRecords > 0) {
-    for (let i = 0; i < numRecords; i++) {
-      const record = {};
-      for (const columnName in columnsConfig) {
-        const config = columnsConfig[columnName];
-        record[columnName] = generateValue(config);
+    } else if (numRecords > 0) {
+      for (let i = 0; i < numRecords; i++) {
+        const record = {};
+        const applyDegradation = Math.random() < degradationProportion;
+        for (const columnName in columnsConfig) {
+          const config = columnsConfig[columnName];
+          config.columnName = columnName; // Add column name to config for reference
+          record[columnName] = generateValue(config, day, applyDegradation);
+        }
+        record.timestamp = generateTimestamp(day);
+        data.push(record);
       }
-      record.timestamp = generateTimestamp();
-      data.push(record);
     }
+
+    const outputFilePath = path.join(outputFolder, `data_${dateString}.json`);
+    fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 2));
+    console.log(`Generated ${data.length} dummy records for ${dateString} and saved to ${outputFilePath}`);
   }
-  fs.writeFileSync(outputFilePath, JSON.stringify(data, null, 2));
-  console.log(
-    `Generated ${data.length} dummy records and saved to ${outputFilePath}`
-  );
 }
 
 // Define the file paths
@@ -111,42 +133,84 @@ const applianceMap = createApplianceMap(applianceFiles);
 // Example usage:
 const dishwasherColConfig = {
   "Serial Number": applianceMap["Dishwasher"], // Array of values
-  Temperature: { range: { min: 68, max: 78, type: 'float' } }, // Range with default outlier probability
-  Humidity: { range: { min: 30, max: 60, outlierProbability: 0.05, type: 'float' } }, // Range with custom outlier probability
+  Temperature: { range: { min: 68, max: 78, type: "float" } }, // Range with default outlier probability
+  Humidity: {
+    range: { min: 30, max: 60, outlierProbability: 0.05, type: "float" }
+  }, // Range with custom outlier probability
   Status: ["On", "Off"] // Array of values
 };
 
 const hvacColConfig = {
   "Serial Number": applianceMap["Dishwasher"], // Array of values
-  TemperatureAccuracy: { range: { min: -2, max: 2, outlierProbability: 0.05, type: 'float' } }, 
-  Airflow: { range: { min: 900, max: 1800, outlierProbability: 0.05, type: 'int' } },
-  MaxDecibels: { range: { min: 0, max: 60, outlierProbability: 0.05, type: 'int' } },
-  EnergyConsumption: { range: { min: 3, max: 4.5, outlierProbability: 0.05, type: 'float' } },
-  FilterDifferential: { range: { min: 0.1, max: 0.3, outlierProbability: 0.05, type: 'float' } },
-  RefrigerantLevels: { range: { min: 0, max: 1, outlierProbability: 0.05, type: 'float' } },
-  DuctPressure: { range: { min: 0.3, max: 0.8, outlierProbability: 0.05, type: 'float' } }
+  TemperatureAccuracy: {
+    range: { min: -2, max: 2, outlierProbability: 0.05, type: "float" }
+  },
+  Airflow: {
+    range: { min: 900, max: 1800, outlierProbability: 0.05, type: "int" }
+  },
+  MaxDecibels: {
+    range: { min: 0, max: 60, outlierProbability: 0.05, type: "int" }
+  },
+  EnergyConsumption: {
+    range: { min: 3, max: 4.5, outlierProbability: 0.05, type: "float" }
+  },
+  FilterDifferential: {
+    range: { min: 0.1, max: 0.3, outlierProbability: 0.05, type: "float" }
+  },
+  RefrigerantLevels: {
+    range: { min: 0, max: 1, outlierProbability: 0.05, type: "float" }
+  },
+  DuctPressure: {
+    range: { min: 0.3, max: 0.8, outlierProbability: 0.05, type: "float" }
+  }
 };
 
 const refrigeratorColConfig = {
   "Serial Number": applianceMap["Refrigerator"], // Array of values
-  FridgeTemperature: { range: { min: 35, max: 40, outlierProbability: 0.05, type: 'float' } },
-  FreezerTemperature: { range: { min: 0, max: 5, outlierProbability: 0.05, type: 'float' } },
-  MaxDecibels: { range: { min: 0, max: 40, outlierProbability: 0.05, type: 'int' } },
-  EnergyConsumption: { range: { min: 0.5, max: 1.5, outlierProbability: 0.01, type: 'float' } },
-  DefrostCycleFrequency: { range: { min: 6, max: 8, outlierProbability: 0.05, type: 'int' } }
+  FridgeTemperature: {
+    range: { min: 35, max: 40, outlierProbability: 0.05, type: "float" }
+  },
+  FreezerTemperature: {
+    range: { min: 0, max: 5, outlierProbability: 0.05, type: "float" }
+  },
+  MaxDecibels: {
+    range: { min: 0, max: 40, outlierProbability: 0.05, type: "int" }
+  },
+  EnergyConsumption: {
+    range: { min: 0.5, max: 1.5, outlierProbability: 0.01, type: "float" }
+  },
+  DefrostCycleFrequency: {
+    range: { min: 6, max: 8, outlierProbability: 0.05, type: "int" }
+  }
 };
 
 const washerColConfig = {
   "Serial Number": applianceMap["Washer"], // Array of values
-  WaterUsage: { range: { min: 15, max: 20, outlierProbability: 0.01, type: 'int' } },
-  MaxWaterTemperature: { range: { min: 40, max: 140, outlierProbability: 0.05, type: 'int' } },
-  SpinSpeed: { range: { min: 600, max: 1400, outlierProbability: 0.05, type: 'int' } },
-  MaxDecibels: { range: { min: 50, max: 70, outlierProbability: 0.05, type: 'int' } },
-  EnergyConsumption: { range: { min: 0.2, max: 1.5, outlierProbability: 0.05, type: 'float' } },
-  VibrationLevel: { range: { min: 0.5, max: 1.0, outlierProbability: 0.05, type: 'float' } },
+  WaterUsage: {
+    range: { min: 15, max: 20, outlierProbability: 0.01, type: "int" }
+  },
+  MaxWaterTemperature: {
+    range: { min: 40, max: 140, outlierProbability: 0.05, type: "int" }
+  },
+  SpinSpeed: {
+    range: { min: 600, max: 1400, outlierProbability: 0.05, type: "int" }
+  },
+  MaxDecibels: {
+    range: { min: 50, max: 70, outlierProbability: 0.05, type: "int" }
+  },
+  EnergyConsumption: {
+    range: { min: 0.2, max: 1.5, outlierProbability: 0.05, type: "float" }
+  },
+  VibrationLevel: {
+    range: { min: 0.5, max: 1.0, outlierProbability: 0.05, type: "float" }
+  }
 };
 
-const parentFolder = 'data/s3/';
-generateDummyData(hvacColConfig, parentFolder + "hvac.json", {
-  keyAttribute: "Serial Number"
+const parentFolder = "data/s3/";
+generateDummyData(hvacColConfig, parentFolder, {
+  keyAttribute: "Serial Number",
+  degradationDays: 30,
+  degradationColumns: ["FilterDifferential", "EnergyConsumption"],
+  degradationProportion: 0.1,
+  timespanDays: 30
 });
